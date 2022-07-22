e69d78da3b7835f0cd4dbf633d63dc5a796dc247
canonn11 <myungkwanokk@gmail.com>
2022-07-22 12:15:51 +0900

    공용 컴포넌트 수정

--- src/lib/Back.svelte
+++ src/lib/Back.svelte
@@ -2,8 +2,10 @@
 	import { goto } from '$app/navigation';
 	import Icon from '$lib/Icon.svelte';
 	export let url = null;
+	export let icon = null;
 </script>
 
+{#if icon === 'arrow'}
 <button
 	type="button"
 	class="absolute left-4 top-4 "
@@ -14,3 +16,14 @@
 >
 	<Icon icon="arrow-left" size={24} />
 </button>
+{:else}
+<button
+	type="button"
+	class="absolute left-4 top-4 "
+	on:click={() => {
+		console.log(window.history);
+		return url ? goto(url) : window.history.go(-1);
+	}}
+>
+	<Icon icon="x" size={24} />
+</button>{/if}
--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -8,46 +8,47 @@
 	class="fixed sm:mx-auto sm:max-w-sm bottom-0 left-0 right-0 grid grid-cols-5 bg-white border-t h-15"
 >
 	<a
+		href="/items/cetegory"
+		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/items/category'
+			? 'text-blue-500'
+			: 'text-gray-800'}"
+	>
+		<Icon icon="align-justify" size={24} />
+		<span class="text-xs mt-1 font-bold">카테고리</span>
+	</a>
+	<a
+		href="/items/search"
+		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/items/search'
+			? 'text-blue-500'
+			: 'text-gray-800'}"
+	>
+		<Icon icon="search" size={24} />
+		<span class="text-xs mt-1 font-bold">검색</span>
+	</a>
+	<a
 		href="/"
 		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/'
-			? 'text-indigo-600'
+			? 'text-blue-500'
 			: 'text-gray-800'}"
 	>
 		<Icon icon="home" size={24} />
 		<span class="text-xs mt-1 font-bold">홈</span>
 	</a>
 	<a
-		href="/users"
-		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/users'
-			? 'text-indigo-600'
+		href="/orders/cart"
+		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/orders/cart'
+			? 'text-blue-500'
 			: 'text-gray-800'}"
-	>
-		<Icon icon="users" size={24} />
-		<span class="text-xs mt-1 font-bold">전문가</span>
-	</a>
-	<a
-		href="/items/new"
-		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/items/new'
-			? 'text-indigo-600'
-			: 'text-gray-800'}"
-	>
-		<Icon icon="plus" size={24} />
-		<span class="text-xs mt-1 font-bold">요청하기</span>
-	</a>
-	<a
-		href="/chats"
-		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/chats'
-			? 'text-indigo-600'
-			: 'text-gray-800'}"
-		><Icon icon="message-circle" size={24} />
-		<span class="text-xs mt-1 font-bold">채팅</span>
+		><Icon icon="shopping-cart" size={24} />
+		<span class="text-xs mt-1 font-bold">장바구니</span>
 	</a>
 	<a
 		href="/users/mypage"
 		class="flex flex-col items-center py-2 hover {$page.url.pathname === '/users/mypage'
-			? 'text-indigo-600'
+			? 'text-blue-500'
 			: 'text-gray-800'}"
-		><Icon icon="user" size={24} />
-		<span class="text-xs mt-1 font-bold">더보기</span>
+		><Icon icon="smile" size={24} />
+		<span class="text-xs mt-1 font-bold">내 정보</span>
 	</a>
 </div>
+
